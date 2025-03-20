d685f5fa59e07ab449e3fcf629b3bd21c9287711
gaoyuan.1226 <gaoyuan.1226@bytedance.com>
2025-03-20 19:17:11 +0800

    test: add source code test case

--- tests/isolate/test/index.test.ts
+++ tests/isolate/test/index.test.ts
@@ -1,4 +1,5 @@
 import { describe, expect, it } from '@rstest/core';
+import { getCount, increment } from '../src/index';
 
 process.env.index = '1';
 globalThis.index = '1';
@@ -14,5 +15,10 @@ describe('Test isolate', () => {
     await new Promise((resolve) => setTimeout(resolve, 1000));
     expect(globalThis.index).toBe('1');
     expect(globalThis.index1).toBeUndefined();
+  });
+
+  it('should call source code isolate', async () => {
+    increment();
+    expect(getCount()).toBe(1);
   });
 });
--- tests/isolate/test/index1.test.ts
+++ tests/isolate/test/index1.test.ts
@@ -1,4 +1,5 @@
 import { describe, expect, it } from '@rstest/core';
+import { getCount, increment } from '../src/index';
 
 process.env.index1 = '1';
 globalThis.index1 = '1';
@@ -14,5 +15,10 @@ describe('Test isolate - 1', () => {
     await new Promise((resolve) => setTimeout(resolve, 1000));
     expect(globalThis.index1).toBe('1');
     expect(globalThis.index).toBeUndefined();
+  });
+
+  it('should call source code isolate', async () => {
+    increment();
+    expect(getCount()).toBe(1);
   });
 });
