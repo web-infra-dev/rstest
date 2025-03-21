9c48e35d273509021e0f53db44a12f3a78f0057a
gaoyuan.1226 <gaoyuan.1226@bytedance.com>
2025-03-21 11:30:46 +0800

    test: add ut test

---
+++ packages/core/tests/utils/entries.test.ts
@@ -0,0 +1,25 @@
+import path from 'node:path';
+import { describe, expect, it } from '@rstest/core';
+import { filterFiles } from '../../src/utils/entries';
+
+describe('test filterFiles', () => {
+  it('should filter files correctly', () => {
+    const testFiles = ['index.test.ts', 'index1.test.ts', 'index2.test.ts'].map(
+      (filename) => path.join(__dirname, filename),
+    );
+
+    expect(filterFiles(testFiles, ['index.test.ts'], __dirname)).toEqual([
+      testFiles[0],
+    ]);
+
+    expect(
+      filterFiles(
+        testFiles,
+        [path.join(__dirname, 'index.test.ts')],
+        __dirname,
+      ),
+    ).toEqual([testFiles[0]]);
+
+    expect(filterFiles(testFiles, ['index'], __dirname)).toEqual(testFiles);
+  });
+});
