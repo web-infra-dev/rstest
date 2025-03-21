98a898aa79ea05ff07950ad4f595baef94c25963
gaoyuan.1226 <gaoyuan.1226@bytedance.com>
2025-03-21 15:42:40 +0800

    chore: format error

--- packages/core/src/utils/helper.ts
+++ packages/core/src/utils/helper.ts
@@ -5,4 +5,28 @@ export function getAbsolutePath(base: string, filepath: string): string {
   return isAbsolute(filepath) ? filepath : join(base, filepath);
 }
 
+export const isPlainObject = (obj: unknown): obj is Record<string, any> => {
+  return (
+    obj !== null &&
+    typeof obj === 'object' &&
+    Object.getPrototypeOf(obj) === Object.prototype
+  );
+};
+
+export function formatError(error: unknown): Error | string {
+  if (typeof error === 'string' || error instanceof Error) {
+    return error;
+  }
+
+  if (isPlainObject(error) && error.message) {
+    const e = new Error(error.name || 'unknown error');
+    e.message = error.message;
+    e.stack = error.stack;
+
+    return e;
+  }
+
+  return String(error);
+}
+
 export { color };
