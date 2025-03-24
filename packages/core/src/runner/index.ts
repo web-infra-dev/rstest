3972270dbeb41b595021c0705db316ccb214fdee
9aoy <gaoyuan.1226@bytedance.com>
2025-03-24 17:25:27 +0800

    refactor: split TestRunner & RunnerRuntime (#43)

--- packages/core/src/runner/index.ts
+++ packages/core/src/runner/index.ts
@@ -1,1 +1,37 @@
-export * from './runner';
+import type { RunnerAPI, TestAPI, TestResult } from '../types';
+
+import { TestRunner } from './runner';
+import { RunnerRuntime } from './runtime';
+
+export function createRunner(): {
+  api: RunnerAPI;
+  runner: {
+    runTest: (testFilePath: string) => Promise<TestResult>;
+    getCurrentTest: RunnerRuntime['getCurrentTest'];
+  };
+} {
+  const runtimeAPI: RunnerRuntime = new RunnerRuntime();
+  const testRunner: TestRunner = new TestRunner();
+
+  const describe: (description: string, fn: () => void) => void =
+    runtimeAPI.describe.bind(runtimeAPI);
+  const it = runtimeAPI.it.bind(runtimeAPI) as TestAPI;
+
+  it.fails = runtimeAPI.fails.bind(runtimeAPI);
+  it.todo = runtimeAPI.todo.bind(runtimeAPI);
+  it.skip = runtimeAPI.skip.bind(runtimeAPI);
+
+  return {
+    api: {
+      describe,
+      it,
+      test: it,
+    },
+    runner: {
+      runTest: async (testFilePath: string) => {
+        return testRunner.runTest(runtimeAPI.getTests(), testFilePath);
+      },
+      getCurrentTest: () => runtimeAPI.getCurrentTest(),
+    },
+  };
+}
