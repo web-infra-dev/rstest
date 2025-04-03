debd92b9d7e3256b3e5d0f22a0ecf62ad5bc0778
9aoy <gaoyuan.1226@bytedance.com>
2025-04-03 15:51:25 +0800

    test(cli): add exit code test cases (#72)

---
+++ tests/cli/fixtures/success.test.ts
@@ -0,0 +1,5 @@
+import { expect, it } from '@rstest/core';
+
+it('should succeed', () => {
+  expect(1 + 1).toBe(2);
+});
---
+++ tests/cli/index.test.ts
@@ -0,0 +1,36 @@
+import { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { describe, expect, it } from '@rstest/core';
+import { runRstestCli } from '../scripts/';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+describe('test exit code', () => {
+  it('should return code 0 when test succeed', async () => {
+    const process = await runRstestCli({
+      command: 'rstest',
+      args: ['run', 'success.test.ts'],
+      options: {
+        nodeOptions: {
+          cwd: __dirname,
+        },
+      },
+    });
+
+    expect(process.exitCode).toBe(0);
+  });
+
+  it('should return code 1 when test failed', async () => {
+    const process = await runRstestCli({
+      command: 'rstest',
+      args: ['run', 'fail.test.ts'],
+      options: {
+        nodeOptions: {
+          cwd: __dirname,
+        },
+      },
+    });
+    expect(process.exitCode).toBe(1);
+  });
+});
