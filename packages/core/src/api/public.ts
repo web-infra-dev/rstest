3ed27bcf96a62cdeffbd0f8b652739d8a9f00ccc
9aoy <gaoyuan.1226@bytedance.com>
2025-04-14 10:56:42 +0800

    feat: support beforeAll (#87)

--- packages/core/src/api/public.ts
+++ packages/core/src/api/public.ts
@@ -5,4 +5,5 @@ export declare const expect: Rstest['expect'];
 export declare const it: Rstest['it'];
 export declare const test: Rstest['test'];
 export declare const describe: Rstest['describe'];
+export declare const beforeAll: Rstest['beforeAll'];
 export declare const afterAll: Rstest['afterAll'];
