598e569b256275b4f6a49279714a6dfdee319be1
gaoyuan.1226 <gaoyuan.1226@bytedance.com>
2025-04-18 18:17:09 +0800

    fix: update tests

--- tests/externals/fixtures/index.test.ts
+++ tests/externals/fixtures/index.test.ts
@@ -5,6 +5,11 @@ it('should load esm correctly', () => {
   expect(stripAnsi('\u001B[4mUnicorn\u001B[0m')).toBe('Unicorn');
 });
 
+it('should load esm dynamic correctly', async () => {
+  const { default: stripAnsi } = await import('strip-ansi');
+  expect(stripAnsi('\u001B[4mUnicorn\u001B[0m')).toBe('Unicorn');
+});
+
 it('should load cjs with require correctly', () => {
   const picocolors = require('picocolors');
   expect(picocolors.green).toBeDefined();
