5428a68646457e7d45494309b3aa0c08a01d914f
gaoyuan.1226 <gaoyuan.1226@bytedance.com>
2025-04-22 15:37:05 +0800

    fix: update test

--- tests/snapshot/file.test.ts
+++ tests/snapshot/file.test.ts
@@ -64,4 +64,23 @@ describe('test snapshot', () => {
   it('test toMatchFileSnapshot correctly', async () => {
     expect('hello world').toMatchFileSnapshot('__snapshots__/file.output.txt');
   });
+
+  it('should failed when snapshot unmatched', async () => {
+    const { cli } = await runRstestCli({
+      command: 'rstest',
+      args: ['run', 'fixtures/fail.test.ts'],
+      options: {
+        nodeOptions: {
+          cwd: __dirname,
+        },
+      },
+    });
+
+    await cli.exec;
+    expect(cli.exec.process?.exitCode).toBe(1);
+
+    const logs = cli.stdout.split('\n').filter(Boolean);
+
+    expect(logs.find((log) => log.includes('Snapshots 1 failed'))).toBeTruthy();
+  });
 });
