ce8878ecbeaae559003ff6d0d2eccd5f070ca66e
gaoyuan.1226 <gaoyuan.1226@bytedance.com>
2025-04-23 16:28:42 +0800

    fix: global api

--- packages/core/globals.d.ts
+++ packages/core/globals.d.ts
@@ -3,5 +3,10 @@ declare global {
   const describe: typeof import('@rstest/core')['describe'];
   const it: typeof import('@rstest/core')['it'];
   const expect: typeof import('@rstest/core')['expect'];
+  const beforeAll: typeof import('@rstest/core')['beforeAll'];
+  const afterAll: typeof import('@rstest/core')['afterAll'];
+  const beforeEach: typeof import('@rstest/core')['beforeEach'];
+  const afterEach: typeof import('@rstest/core')['afterEach'];
+  const rstest: typeof import('@rstest/core')['rstest'];
 }
 export {};
