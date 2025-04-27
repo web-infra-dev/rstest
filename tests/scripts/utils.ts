a28c38c54a242835265133b1de4c7d1b1bf8c82b
gaoyuan.1226 <gaoyuan.1226@bytedance.com>
2025-04-27 15:16:43 +0800

    test: use expectFile instead of waitFile

--- tests/scripts/utils.ts
+++ tests/scripts/utils.ts
@@ -1,20 +1,12 @@
 import fs from 'node:fs';
+import { expect } from '@rstest/core';
 
-export const waitFile = async (filePath: string, timeout = 3000) => {
-  return new Promise((resolve, reject) => {
-    const interval = setInterval(() => {
-      if (fs.existsSync(filePath)) {
-        clearInterval(interval);
-        resolve(true);
-      }
-    }, 100);
-
-    setTimeout(() => {
-      clearInterval(interval);
-      reject(new Error(`File ${filePath} not found within ${timeout}ms`));
-    }, timeout);
-  });
-};
+export const expectFile = (filePath: string, timeout = 3000) =>
+  expect
+    .poll(() => fs.existsSync(filePath), {
+      timeout,
+    })
+    .toBeTruthy();
 
 export const sleep = (ms: number) => {
   return new Promise<void>((resolve) => {
