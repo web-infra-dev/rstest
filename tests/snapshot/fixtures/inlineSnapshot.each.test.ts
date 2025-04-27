fe34444c39f8c3489d9649c068a9157355cc2505
gaoyuan.1226 <gaoyuan.1226@bytedance.com>
2025-04-27 14:33:28 +0800

    fix: should throw error when use toMatchInlineSnapshot in test.each

---
+++ tests/snapshot/fixtures/inlineSnapshot.each.test.ts
@@ -0,0 +1,11 @@
+import { describe, expect, it } from '@rstest/core';
+
+describe('test ineSnapshot in each', () => {
+  it.each([
+    { a: 1, b: 1 },
+    { a: 1, b: 2 },
+    { a: 2, b: 1 },
+  ])('add two numbers correctly', ({ a, b }) => {
+    expect(a + b).toMatchInlineSnapshot();
+  });
+});
