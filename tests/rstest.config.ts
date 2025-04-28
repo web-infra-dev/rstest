e11cbf48881a266a04782cb725b005b32a2465a4
9aoy <gaoyuan.1226@bytedance.com>
2025-04-28 15:28:00 +0800

    test: fix timeout in windows CI (#148)

--- tests/rstest.config.ts
+++ tests/rstest.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from '@rstest/core';
 
 export default defineConfig({
   setupFiles: ['./rstest.setup.ts'],
+  testTimeout: process.env.CI ? 10_000 : 5_000,
   exclude: [
     '**/node_modules/**',
     '**/dist/**',
