c7b09292cf5b347d1730121452eda8deab540df4
9aoy <gaoyuan.1226@bytedance.com>
2025-05-09 14:10:21 +0800

    chore: merge runtime fails API (#168)

--- packages/core/src/runtime/runner/runtime.ts
+++ packages/core/src/runtime/runner/runtime.ts
@@ -4,8 +4,13 @@ import type {
   AfterEachListener,
   BeforeAllListener,
   BeforeEachListener,
+  DescribeAPI,
+  DescribeBaseAPI,
   DescribeEachFn,
+  RunnerAPI,
   Test,
+  TestAPI,
+  TestBaseAPI,
   TestCase,
   TestEachFn,
   TestRunMode,
@@ -228,13 +233,21 @@ export class RunnerRuntime {
     }
   }
 
-  it(
-    name: string,
-    fn?: () => void | Promise<void>,
-    timeout: number = this.defaultTestTimeout,
-    runMode: TestRunMode = 'run',
+  it({
+    name,
+    fn,
+    timeout = this.defaultTestTimeout,
+    runMode = 'run',
+    fails = false,
     each = false,
-  ): void {
+  }: {
+    name: string;
+    fn?: () => void | Promise<void>;
+    timeout?: number;
+    runMode?: TestRunMode;
+    each?: boolean;
+    fails?: boolean;
+  }): void {
     this.addTestCase({
       name,
       fn: fn
@@ -249,6 +262,7 @@ export class RunnerRuntime {
       type: 'case',
       timeout,
       each,
+      fails,
     });
   }
 
@@ -282,37 +296,15 @@ export class RunnerRuntime {
         const param = cases[i]!;
         const params = castArray(param) as Parameters<typeof fn>;
 
-        this.it(
-          formatName(name, param, i),
-          () => fn?.(...params),
+        this.it({
+          name: formatName(name, param, i),
+          fn: () => fn?.(...params),
           timeout,
           runMode,
-          true,
-        );
+          each: true,
+        });
       }
     };
-  }
-
-  fails(
-    name: string,
-    fn?: () => void | Promise<void>,
-    timeout: number = this.defaultTestTimeout,
-    runMode: TestRunMode = 'run',
-  ): void {
-    this.addTestCase({
-      name,
-      fn: fn
-        ? wrapTimeout({
-            name: 'test',
-            fn,
-            timeout,
-            stackTraceError: new Error('STACK_TRACE_ERROR'),
-          })
-        : fn,
-      fails: true,
-      runMode,
-      type: 'case',
-    });
   }
 
   private getCurrentSuite(): TestSuite {
@@ -328,3 +320,78 @@ export class RunnerRuntime {
     throw new Error('Expect to find a suite, but got undefined');
   }
 }
+
+export const createRuntimeAPI = ({
+  sourcePath,
+  testTimeout,
+}: { sourcePath: string; testTimeout: number }): {
+  api: RunnerAPI;
+  instance: RunnerRuntime;
+} => {
+  const runtimeInstance: RunnerRuntime = new RunnerRuntime({
+    sourcePath,
+    testTimeout,
+  });
+
+  // TODO: optimize chainable API
+
+  const it = ((name, fn, timeout) =>
+    runtimeInstance.it({ name, fn, timeout })) as TestAPI;
+  it.fails = (name, fn, timeout) =>
+    runtimeInstance.it({ name, fn, timeout, fails: true });
+  it.each = runtimeInstance.each.bind(runtimeInstance) as TestEachFn;
+  it.todo = (name, fn, timeout) =>
+    runtimeInstance.it({ name, fn, timeout, runMode: 'todo' });
+
+  it.skip = ((name, fn, timeout) =>
+    runtimeInstance.it({ name, fn, timeout, runMode: 'skip' })) as TestBaseAPI;
+  it.skip.fails = (name, fn, timeout) =>
+    runtimeInstance.it({ name, fn, timeout, runMode: 'skip', fails: true });
+  it.skip.each = ((cases: any) =>
+    runtimeInstance.each(cases, 'skip')) as TestEachFn;
+
+  it.only = ((name, fn, timeout) =>
+    runtimeInstance.it({ name, fn, timeout, runMode: 'only' })) as TestBaseAPI;
+  it.only.fails = (name, fn, timeout) =>
+    runtimeInstance.it({ name, fn, timeout, runMode: 'only', fails: true });
+  it.only.each = ((cases: any) =>
+    runtimeInstance.each(cases, 'only')) as TestEachFn;
+
+  it.runIf = (condition: boolean) => (condition ? it : it.skip) as TestBaseAPI;
+  it.skipIf = (condition: boolean) => (condition ? it.skip : it) as TestBaseAPI;
+
+  const describe = ((name, fn) =>
+    runtimeInstance.describe(name, fn)) as DescribeAPI;
+
+  describe.only = ((name, fn) =>
+    runtimeInstance.describe(name, fn, 'only')) as DescribeBaseAPI;
+  describe.only.each = ((cases: any) =>
+    runtimeInstance.describeEach(cases, 'only')) as DescribeEachFn;
+  describe.todo = (name, fn) => runtimeInstance.describe(name, fn, 'todo');
+  describe.skip = ((name, fn) =>
+    runtimeInstance.describe(name, fn, 'skip')) as DescribeBaseAPI;
+  describe.skip.each = ((cases: any) =>
+    runtimeInstance.describeEach(cases, 'skip')) as DescribeEachFn;
+
+  describe.skipIf = (condition: boolean) =>
+    (condition ? describe.skip : describe) as DescribeBaseAPI;
+  describe.runIf = (condition: boolean) =>
+    (condition ? describe : describe.skip) as DescribeBaseAPI;
+
+  describe.each = runtimeInstance.describeEach.bind(
+    runtimeInstance,
+  ) as DescribeEachFn;
+
+  return {
+    api: {
+      describe,
+      it,
+      test: it,
+      afterAll: runtimeInstance.afterAll.bind(runtimeInstance),
+      beforeAll: runtimeInstance.beforeAll.bind(runtimeInstance),
+      afterEach: runtimeInstance.afterEach.bind(runtimeInstance),
+      beforeEach: runtimeInstance.beforeEach.bind(runtimeInstance),
+    },
+    instance: runtimeInstance,
+  };
+};
