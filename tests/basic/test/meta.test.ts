1fe116f3842629addeabf8ceb0f853478980686b
9aoy <gaoyuan.1226@bytedance.com>
2025-05-27 11:56:20 +0800

    feat: support import.meta.dirname / filename (#227)

--- tests/basic/test/meta.test.ts
+++ tests/basic/test/meta.test.ts
@@ -7,8 +7,7 @@ describe('Meta', () => {
     expect(__filename.endsWith('/basic/test/meta.test.ts')).toBeTruthy();
   });
 
-  // TODO
-  it.todo('should get test file import meta correctly', async () => {
+  it('should get test file import meta correctly', async () => {
     expect(import.meta.dirname.includes('/basic/test')).toBeTruthy();
     expect(
       import.meta.filename.endsWith('/basic/test/meta.test.ts'),
