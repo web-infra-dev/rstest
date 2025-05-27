d4ca1ad0173c1ace2bcb997211646926582223c4
fi3ework <fi3ework@gmail.com>
2025-05-27 15:37:35 +0800

    revert

--- scripts/rstest.setup.ts
+++ scripts/rstest.setup.ts
@@ -2,10 +2,18 @@ import path from 'node:path';
 import { expect } from '@rstest/core';
 import { createSnapshotSerializer } from 'path-serializer';
 
-console.log('🙄', __dirname);
+console.log(
+  '🙄',
+  __dirname,
+  // remove all \r in __dirname
+  __dirname.replace(/\r/g, ''),
+  path.normalize(__dirname),
+  path.join(__dirname, '..'),
+  path.normalize(path.join(__dirname, '..')),
+);
 
 expect.addSnapshotSerializer(
   createSnapshotSerializer({
-    root: path.join(__dirname, '..'),
+    root: path.normalize(path.join(__dirname, '..')),
   }),
 );
