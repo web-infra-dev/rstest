19232a723c80919006c84a64027507ae65c87a94
fi3ework <fi3ework@gmail.com>
2025-06-19 15:45:03 +0800

    requireMock, CI PASS

--- tests/mock/tests/reduxNotMocked.test.ts
+++ tests/mock/tests/reduxNotMocked.test.ts
@@ -1,6 +1,7 @@
 import { expect, rs, test } from '@rstest/core';
 import * as redux from 'redux';
 
+// importMock
 test('mocked redux', async () => {
   const { default: rx } = await rs.importMock<any>('redux');
   await rx.isAction('string');
@@ -9,4 +10,16 @@ test('mocked redux', async () => {
 
 test('actual redux is not mocked', async () => {
   expect(rs.isMockFunction(redux.isAction)).toBe(false);
+});
+
+// requireMock
+test('mocked redux (CJS)', async () => {
+  const rx = rs.requireMock<any>('redux').default;
+  await rx.isAction('string');
+  expect(rx.isAction).toHaveBeenCalledWith('string');
+});
+
+test('actual redux is not mocked (CJS)', async () => {
+  const rx = require('redux');
+  expect(rs.isMockFunction(rx.isAction)).toBe(false);
 });
