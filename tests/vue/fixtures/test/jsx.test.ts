5e54cd4558446536a08621c9b803e004de3a68d9
gaoyuan.1226 <gaoyuan.1226@bytedance.com>
2025-07-09 19:03:08 +0800

    fix: remove useless ts ignore

--- tests/vue/fixtures/test/jsx.test.ts
+++ tests/vue/fixtures/test/jsx.test.ts
@@ -4,6 +4,7 @@ import App from '../src/App.tsx';
 
 test('should emit clickApp event when button is clicked', async () => {
   const wrapper = mount(App, {});
+  console.log('wrapper', wrapper.html());
   wrapper.find('button').trigger('click');
   expect(wrapper.emitted('clickApp')).toBeTruthy();
 });
