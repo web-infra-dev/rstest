30524f92103cb763b48902584ee64ff012742533
gaoyuan.1226 <gaoyuan.1226@bytedance.com>
2025-07-14 17:50:38 +0800

    fix: skip `module` field when commonjs require

--- tests/externals/fixtures/test-pkg/importModule.ts
+++ tests/externals/fixtures/test-pkg/importModule.ts
@@ -1,4 +1,6 @@
 // @ts-expect-error: the package is alongside, only for testing purposes
 import { a } from 'test-module-field';
 
-export { a };
+const { a: b } = require('test-module-field');
+
+export { a, b };
