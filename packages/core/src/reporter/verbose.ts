66f4fbfbe8bf5b8b5061d2b7a69c6a9bed0f32d5
gaoyuan.1226 <gaoyuan.1226@bytedance.com>
2025-07-30 15:57:49 +0800

    feat: support VerboseReporter

---
+++ packages/core/src/reporter/verbose.ts
@@ -0,0 +1,19 @@
+import { relative } from 'pathe';
+import type { TestFileResult } from '../types';
+import { DefaultReporter } from './index';
+import { logCase, logFileTitle } from './utils';
+
+export class VerboseReporter extends DefaultReporter {
+  override onTestFileResult(test: TestFileResult): void {
+    this.statusRenderer?.removeRunningModule(test.testPath);
+
+    const relativePath = relative(this.rootPath, test.testPath);
+    const { slowTestThreshold } = this.config;
+
+    logFileTitle(test, relativePath, slowTestThreshold, true);
+
+    for (const result of test.results) {
+      logCase(result, slowTestThreshold);
+    }
+  }
+}
