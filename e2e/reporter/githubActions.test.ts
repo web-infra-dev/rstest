af4987cb1b7c8f58bed712b6b4f05de393cdb1cc
9aoy <gaoyuan.1226@bytedance.com>
2025-08-15 16:58:33 +0800

    chore: rename `test/` to `e2e/` (#483)

--- e2e/reporter/githubActions.test.ts
+++ e2e/reporter/githubActions.test.ts
@@ -22,8 +22,8 @@ it('github-actions', async () => {
 
   expect(logs).toMatchInlineSnapshot(`
     [
-      "::error file=<ROOT>/tests/reporter/fixtures/githubActions.test.ts,line=4,col=17,title=fixtures/githubActions.test.ts > should add two numbers correctly::expected 2 to be 4 // Object.is equality%0A- Expected%0A+ Received%0A%0A- 4%0A+ 2",
-      "::error file=<ROOT>/tests/reporter/fixtures/githubActions.test.ts,line=8,col=19,title=fixtures/githubActions.test.ts > test snapshot::Snapshot \`test snapshot 1\` mismatched%0A- Expected%0A+ Received%0A%0A- "hello world"%0A+ "hello"",
+      "::error file=<ROOT>/e2e/reporter/fixtures/githubActions.test.ts,line=4,col=17,title=fixtures/githubActions.test.ts > should add two numbers correctly::expected 2 to be 4 // Object.is equality%0A- Expected%0A+ Received%0A%0A- 4%0A+ 2",
+      "::error file=<ROOT>/e2e/reporter/fixtures/githubActions.test.ts,line=8,col=19,title=fixtures/githubActions.test.ts > test snapshot::Snapshot \`test snapshot 1\` mismatched%0A- Expected%0A+ Received%0A%0A- "hello world"%0A+ "hello"",
     ]
   `);
 });
