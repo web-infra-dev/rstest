c08fdbcc8b7ced831c9dd37a611ff1108252fece
gaoyuan.1226 <gaoyuan.1226@bytedance.com>
2025-08-19 17:24:40 +0800

    test: split shortcuts case to fix timeout

--- e2e/rstest.config.ts
+++ e2e/rstest.config.ts
@@ -13,7 +13,6 @@ export default defineConfig({
     '**/node_modules/**',
     '**/dist/**',
     '**/fixtures/**',
-    '**/fixtures-test/**',
-    '**/fixtures-test-*/**',
+    '**/fixtures-*/**',
   ],
 });
--- e2e/watch/shortcuts.test.ts
+++ e2e/watch/shortcuts.test.ts
@@ -8,39 +8,15 @@ const __dirname = path.dirname(__filename);
 
 describe('CLI shortcuts', () => {
   it('CLI shortcuts should display and work as expected', async () => {
-    const fixturesTargetPath = `${__dirname}/fixtures-test-2`;
-    const { fs } = await prepareFixtures({
-      fixturesPath: `${__dirname}/fixtures`,
+    const fixturesTargetPath = `${__dirname}/fixtures-test-shortcuts`;
+    await prepareFixtures({
+      fixturesPath: `${__dirname}/fixtures-shortcuts`,
       fixturesTargetPath,
     });
 
-    const configFile = path.join(fixturesTargetPath, 'rstest.config.ts');
-
-    fs.update(
-      configFile,
-      (content) => `${content}
-    process.stdin.isTTY = true
-    process.stdin.setRawMode = () => process.stdin;
-`,
-    );
-
-    const testIndex1File = path.join(fixturesTargetPath, 'index1.test.ts');
-
-    fs.create(
-      testIndex1File,
-      `import { describe, expect, it } from '@rstest/core';
-
-      describe('index1', () => {
-        it('should add two numbers correctly', () => {
-          expect(1 + 1).toBe(3);
-        });
-      });
-      `,
-    );
-
     const { cli } = await runRstestCli({
       command: 'rstest',
-      args: ['watch', '--disableConsoleIntercept', '--reporter=default'],
+      args: ['watch'],
       options: {
         nodeOptions: {
           env: {
@@ -70,6 +46,36 @@ describe('CLI shortcuts', () => {
     cli.exec.process!.stdin!.write('a');
     await cli.waitForStdout('Duration');
     expect(cli.stdout).toMatch('Tests 1 failed | 1 passed');
+
+    cli.exec.kill();
+  });
+
+  it('shortcut `f` should works as expected', async () => {
+    const fixturesTargetPath = `${__dirname}/fixtures-test-shortcuts-f`;
+    await prepareFixtures({
+      fixturesPath: `${__dirname}/fixtures-shortcuts`,
+      fixturesTargetPath,
+    });
+
+    const { cli } = await runRstestCli({
+      command: 'rstest',
+      args: ['watch'],
+      options: {
+        nodeOptions: {
+          env: {
+            FORCE_TTY: 'true',
+            CI: undefined,
+          },
+          cwd: fixturesTargetPath,
+        },
+      },
+    });
+
+    // initial run
+    await cli.waitForStdout('Duration');
+    expect(cli.stdout).toMatch('Tests 1 failed | 1 passed');
+
+    await cli.waitForStdout('press h to show help');
 
     cli.resetStd();
 
