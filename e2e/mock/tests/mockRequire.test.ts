738bd8b7722757e0e8f1ebb91cbe7ebcc7b0bb44
gaoyuan.1226 <gaoyuan.1226@bytedance.com>
2025-08-20 15:15:43 +0800

    fix: bump rsbuild

--- e2e/mock/tests/mockRequire.test.ts
+++ e2e/mock/tests/mockRequire.test.ts
@@ -6,6 +6,5 @@ it('mocked redux', () => {
   const redux = require('redux').default;
   redux.isAction('string');
   expect(redux.isAction).toHaveBeenCalledWith('string');
-  // @ts-expect-error
   expect(redux.mocked).toBe('redux_yes');
 });
