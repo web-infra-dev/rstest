736c3c239204e44e42e61633033c3f173f8256d4
gaoyuan.1226 <gaoyuan.1226@bytedance.com>
2025-08-21 15:51:56 +0800

    test: cleanup child process when test finished

--- e2e/scripts/index.ts
+++ e2e/scripts/index.ts
@@ -1,5 +1,6 @@
 import fs from 'node:fs';
 import path from 'node:path';
+import { onTestFinished } from '@rstest/core';
 import stripAnsi from 'strip-ansi';
 import type { Options, Result } from 'tinyexec';
 import { x } from 'tinyexec';
@@ -90,6 +91,10 @@ export async function runRstestCli({
     },
   } as Options);
   const cli = new Cli(process);
+
+  onTestFinished(() => {
+    cli.exec.kill();
+  });
 
   const expectExecSuccess = async () => {
     await cli.exec;
