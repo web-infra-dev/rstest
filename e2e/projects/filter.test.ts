f1b127f2dd9ea0f44de2119e4a547b472bb0fe8b
gaoyuan.1226 <gaoyuan.1226@bytedance.com>
2025-08-28 16:37:17 +0800

    feat: support filter projects via `--project` option

---
+++ e2e/projects/filter.test.ts
@@ -0,0 +1,28 @@
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { describe, expect, it } from '@rstest/core';
+import { runRstestCli } from '../scripts';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+describe('test projects filter', () => {
+  it('should run test success with project filter', async () => {
+    const { cli, expectExecSuccess } = await runRstestCli({
+      command: 'rstest',
+      args: ['run', '--project', 'node', '--globals'],
+      options: {
+        nodeOptions: {
+          cwd: join(__dirname, 'fixtures'),
+        },
+      },
+    });
+
+    await expectExecSuccess();
+    const logs = cli.stdout.split('\n').filter(Boolean);
+
+    // test log print
+    expect(logs.find((log) => log.includes('node/test/index'))).toBeTruthy();
+    expect(logs.find((log) => log.includes('react/test/index'))).toBeFalsy();
+  });
+});
