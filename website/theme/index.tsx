77f43eb1b3e0dcdcdca8d567365c707cd8961b4b
gaoyuan.1226 <gaoyuan.1226@bytedance.com>
2025-09-02 18:16:06 +0800

    docs: llms UI display

--- website/theme/index.tsx
+++ website/theme/index.tsx
@@ -1,7 +1,36 @@
-import { Layout as BaseLayout } from '@rspress/core/theme';
+import {
+  Layout as BaseLayout,
+  getCustomMDXComponent as basicGetCustomMDXComponent,
+} from '@rspress/core/theme';
+import {
+  LlmsContainer,
+  LlmsCopyButton,
+  LlmsViewOptions,
+} from '@rspress/plugin-llms/runtime';
 import { NavIcon } from '@rstack-dev/doc-ui/nav-icon';
+
 import { HomeLayout } from './pages';
 import './index.scss';
+
+export function getCustomMDXComponent() {
+  const { h1: H1, ...mdxComponents } = basicGetCustomMDXComponent();
+
+  const MyH1 = ({ ...props }) => {
+    return (
+      <>
+        <H1 {...props} />
+        <LlmsContainer>
+          <LlmsCopyButton />
+          <LlmsViewOptions />
+        </LlmsContainer>
+      </>
+    );
+  };
+  return {
+    ...mdxComponents,
+    h1: MyH1,
+  };
+}
 
 const Layout = () => {
   return <BaseLayout beforeNavTitle={<NavIcon />} />;
