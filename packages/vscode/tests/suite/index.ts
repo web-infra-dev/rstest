87163fa720147668cb016a419d5c301b3d729235
fi3ework <fi3ework@gmail.com>
2025-09-05 17:56:59 +0800

    wip: add test

---
+++ packages/vscode/tests/suite/index.ts
@@ -0,0 +1,40 @@
+import glob from 'glob';
+import Mocha from 'mocha';
+import path from 'path';
+
+export function run(): Promise<void> {
+  // Create the mocha test
+  const mocha = new Mocha({
+    ui: 'tdd',
+    timeout: 20_000, // 20 seconds
+  });
+
+  const testsRoot = path.resolve(__dirname, '..');
+
+  return new Promise((c, e) => {
+    glob('**/**.test.js', { cwd: testsRoot }, (err, files) => {
+      if (err) {
+        return e(err);
+      }
+
+      // Add files to the test suite
+      for (const f of files) {
+        mocha.addFile(path.resolve(testsRoot, f));
+      }
+
+      try {
+        // Run the mocha test
+        mocha.run((failures) => {
+          if (failures > 0) {
+            e(new Error(`${failures} tests failed.`));
+          } else {
+            c();
+          }
+        });
+      } catch (err) {
+        console.error(err);
+        e(err);
+      }
+    });
+  });
+}
