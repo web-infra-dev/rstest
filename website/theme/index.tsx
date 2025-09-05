b4e878296b5e390197f1e5d30cd534f94cb34d88
Wei <fi3ework@gmail.com>
2025-09-05 15:55:54 +0800

    docs: use Algolia search (#541)

--- website/theme/index.tsx
+++ website/theme/index.tsx
@@ -1,7 +1,12 @@
+import { useLang } from '@rspress/core/runtime';
 import {
   Layout as BaseLayout,
   getCustomMDXComponent as basicGetCustomMDXComponent,
 } from '@rspress/core/theme';
+import {
+  Search as PluginAlgoliaSearch,
+  ZH_LOCALES,
+} from '@rspress/plugin-algolia/runtime';
 import {
   LlmsContainer,
   LlmsCopyButton,
@@ -32,10 +37,27 @@ export function getCustomMDXComponent() {
   };
 }
 
+const Search = () => {
+  const lang = useLang();
+  return (
+    <PluginAlgoliaSearch
+      docSearchProps={{
+        appId: 'TRUZL3HFAU', // cspell:disable-line
+        apiKey: '3cf720e0589287b96f68a8c7bad7f682', // cspell:disable-line
+        indexName: 'rstest',
+        searchParameters: {
+          facetFilters: [`lang:${lang}`],
+        },
+      }}
+      locales={ZH_LOCALES}
+    />
+  );
+};
+
 const Layout = () => {
   return <BaseLayout beforeNavTitle={<NavIcon />} />;
 };
 
-export { Layout, HomeLayout };
+export { Search, Layout, HomeLayout };
 
 export * from '@rspress/core/theme';
