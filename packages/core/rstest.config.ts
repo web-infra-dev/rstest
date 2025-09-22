ef3995143af0965377389c825ffee3199d31732a
gaoyuan.1226 <gaoyuan.1226@bytedance.com>
2025-09-22 15:44:59 +0800

    feat: support use `<rootDir>` in path-based configuration settings

--- packages/core/rstest.config.ts
+++ packages/core/rstest.config.ts
@@ -2,5 +2,11 @@ import { defineConfig } from '@rstest/core';
 
 export default defineConfig({
   setupFiles: ['../../scripts/rstest.setup.ts'],
+  include: ['<rootDir>/tests/**/*.test.ts'],
   globals: true,
+  source: {
+    define: {
+      RSTEST_VERSION: JSON.stringify('0.0.0'),
+    },
+  },
 });
