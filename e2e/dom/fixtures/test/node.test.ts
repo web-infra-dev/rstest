9fff7e388cec82dd461759c0fdf3482e6dcdded9
gaoyuan.1226 <gaoyuan.1226@bytedance.com>
2025-09-29 14:42:38 +0800

    fix: update

--- e2e/dom/fixtures/test/node.test.ts
+++ e2e/dom/fixtures/test/node.test.ts
@@ -1,6 +1,6 @@
-// @ts-expect-error
-import _http_common from 'node:_http_common';
 import { describe, expect, it } from '@rstest/core';
+// @ts-expect-error node builtin module
+import _http_common from '_http_common';
 
 describe('node built-in modules', () => {
   it('should load node built-in modules correctly', () => {
