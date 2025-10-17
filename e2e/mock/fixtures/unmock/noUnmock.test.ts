0927b20a7b6880883f5e27e4e07da69f58b851e5
fi3ework <fi3ework@gmail.com>
2025-10-17 14:28:32 +0800

    test

--- e2e/mock/fixtures/unmock/noUnmock.test.ts
+++
@@ -1,6 +0,0 @@
-import { randomFill } from 'node:crypto';
-import { expect, it } from '@rstest/core';
-
-it('should run setup file correctly', () => {
-  expect(randomFill).toBe('mocked_randomFill');
-});
--- e2e/mock/fixtures/unmock/unmock.test.ts
+++
@@ -1,8 +0,0 @@
-import crypto from 'node:crypto';
-import { expect, it, rs } from '@rstest/core';
-
-rs.unmock('node:crypto');
-
-it('should run setup file correctly2', () => {
-  expect(typeof crypto.randomFill === 'function').toBe(true);
-});
