e07bc3d56ba46f197aeafc65a8eaed85a3598c5d
gaoyuan.1226 <gaoyuan.1226@bytedance.com>
2025-11-07 20:54:54 +0800

    fix: skip windows

--- e2e/runner/test/signalListener.test.ts
+++ e2e/runner/test/signalListener.test.ts
@@ -2,7 +2,7 @@ import { join } from 'node:path';
 import { it } from '@rstest/core';
 import { runRstestCli } from '../../scripts/';
 
-it.skipIf(process.platform !== 'win32')(
+it.skipIf(process.platform === 'win32')(
   'should catch signal exit correctly when signal listener exists',
   async () => {
     const { expectExecFailed, expectLog } = await runRstestCli({
