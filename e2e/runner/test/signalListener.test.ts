d8fe7896898f42142cd20c748a12b92e95f1d52e
gaoyuan.1226 <gaoyuan.1226@bytedance.com>
2025-11-07 20:47:54 +0800

    fix: skip windows

--- e2e/runner/test/signalListener.test.ts
+++ e2e/runner/test/signalListener.test.ts
@@ -2,17 +2,20 @@ import { join } from 'node:path';
 import { it } from '@rstest/core';
 import { runRstestCli } from '../../scripts/';
 
-it('should catch signal exit correctly when signal listener exists', async () => {
-  const { expectExecFailed, expectLog } = await runRstestCli({
-    command: 'rstest',
-    args: ['run', 'signalListener.test.ts'],
-    options: {
-      nodeOptions: {
-        cwd: join(__dirname, 'fixtures'),
+it.skipIf(process.platform !== 'win32')(
+  'should catch signal exit correctly when signal listener exists',
+  async () => {
+    const { expectExecFailed, expectLog } = await runRstestCli({
+      command: 'rstest',
+      args: ['run', 'signalListener.test.ts'],
+      options: {
+        nodeOptions: {
+          cwd: join(__dirname, 'fixtures'),
+        },
       },
-    },
-  });
-  await expectExecFailed();
+    });
+    await expectExecFailed();
 
-  expectLog(/Rstest exited unexpectedly with code 0/);
-});
+    expectLog(/Rstest exited unexpectedly with code 0/);
+  },
+);
