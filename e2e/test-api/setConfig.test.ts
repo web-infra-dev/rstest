ff8c7225a2541211637703db11c40523105af6b4
gaoyuan.1226 <gaoyuan.1226@bytedance.com>
2025-11-18 16:10:22 +0800

    feat: support get runtime config via rs.getConfig API

--- e2e/test-api/setConfig.test.ts
+++ e2e/test-api/setConfig.test.ts
@@ -1,9 +1,9 @@
-import { describe, expect, it } from '@rstest/core';
+import { describe, it } from '@rstest/core';
 import { runRstestCli } from '../scripts';
 
-describe('setConfig', () => {
+describe('setConfig & getConfig', () => {
   it('should throw timeout error when test timeout', async () => {
-    const { cli } = await runRstestCli({
+    const { expectExecFailed, expectLog } = await runRstestCli({
       command: 'rstest',
       args: ['run', 'fixtures/setConfig.test'],
       options: {
@@ -13,13 +13,8 @@ describe('setConfig', () => {
       },
     });
 
-    await cli.exec;
-    expect(cli.exec.process?.exitCode).toBe(1);
-    const logs = cli.stdout.split('\n').filter(Boolean);
-
-    expect(
-      logs.find((log) => log.includes('Error: test timed out in 50ms')),
-    ).toBeTruthy();
-    expect(logs.find((log) => log.includes('Tests 2 failed'))).toBeTruthy();
+    await expectExecFailed();
+    expectLog(/Error: test timed out in 100ms/);
+    expectLog(/Tests 2 failed | 1 passed/);
   });
 });
