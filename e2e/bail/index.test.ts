ff0ed798f6d74681a5e6df90ca910ccac3f2202d
9aoy <9aoyuao@gmail.com>
2025-11-26 16:11:42 +0800

    chore: support `--bail` without value

--- e2e/bail/index.test.ts
+++ e2e/bail/index.test.ts
@@ -7,7 +7,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 describe('test bail option', () => {
-  it('should not run all tests when bail option is set', async () => {
+  it('should not run all tests when `--bail <number>`', async () => {
     const { expectExecFailed, cli } = await runRstestCli({
       command: 'rstest',
       args: ['run', 'fixtures/index.test.ts', '--bail', '1'],
@@ -24,5 +24,43 @@ describe('test bail option', () => {
     // `Tests 1 failed | 1 passed (2)` => 2
     const totalCount = Number(logs[0]!.match(/\((\d+)\)/)?.[1]);
     expect(totalCount).toBe(2);
+  });
+
+  it('should not run all tests when `--bail`', async () => {
+    const { expectExecFailed, cli } = await runRstestCli({
+      command: 'rstest',
+      args: ['run', 'fixtures/index.test.ts', '--bail'],
+      options: {
+        nodeOptions: {
+          cwd: __dirname,
+        },
+      },
+    });
+
+    await expectExecFailed();
+
+    const logs = cli.stdout.split('\n').filter((log) => log.includes('Tests'));
+    // `Tests 1 failed | 1 passed (2)` => 2
+    const totalCount = Number(logs[0]!.match(/\((\d+)\)/)?.[1]);
+    expect(totalCount).toBe(2);
+  });
+
+  it('should run all tests when `--bail 0`', async () => {
+    const { expectExecFailed, cli } = await runRstestCli({
+      command: 'rstest',
+      args: ['run', 'fixtures/index.test.ts', '--bail', '0'],
+      options: {
+        nodeOptions: {
+          cwd: __dirname,
+        },
+      },
+    });
+
+    await expectExecFailed();
+
+    const logs = cli.stdout.split('\n').filter((log) => log.includes('Tests'));
+    // `Tests 1 failed | 2 passed (3)` => 3
+    const totalCount = Number(logs[0]!.match(/\((\d+)\)/)?.[1]);
+    expect(totalCount).toBe(3);
   });
 });
