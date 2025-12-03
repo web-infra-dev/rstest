6abf780a3a44cf2ecbb5062fbfc423bed3a4374f
9aoy <9aoyuao@gmail.com>
2025-12-03 14:27:27 +0800

    fix: stripping types error under `node_modules` (#730)

--- e2e/externals/fixtures/bundle.test.ts
+++ e2e/externals/fixtures/bundle.test.ts
@@ -1,6 +1,6 @@
 import { expect, it } from '@rstest/core';
 import { a } from './test-pkg/testBundle';
 
-it('should load pkg correctly with bundled', () => {
+it('should load typescript pkg correctly', () => {
   expect(a).toBe(1);
 });
