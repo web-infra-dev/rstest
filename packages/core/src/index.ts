f2b03a19834c752b842680386db156a2f52a68aa
9aoy <9aoyuao@gmail.com>
2025-12-16 11:58:34 +0800

    feat: export `mergeProjectConfig` method

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -8,7 +8,7 @@ import type {
 } from './types';
 
 export { initCli, runCLI } from './cli';
-export { loadConfig, mergeRstestConfig } from './config';
+export { loadConfig, mergeProjectConfig, mergeRstestConfig } from './config';
 export { createRstest } from './core';
 
 export * from './runtime/api/public';
