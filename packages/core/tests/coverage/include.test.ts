df590ff28ef916b8586d958cc0fbf4ef0f1f687f
claneo <claneo@outlook.com>
2026-01-04 10:05:01 +0800

    fix windows test cases

--- packages/core/tests/coverage/include.test.ts
+++ packages/core/tests/coverage/include.test.ts
@@ -11,115 +11,88 @@ describe('getIncludedFiles', () => {
     defaultExclude = withDefaultConfig({}).coverage.exclude;
     memfs = fs as any;
 
+    // There's a bug in tinyglobby: the computed common root path cannot be the root of the Windows volume.
     [
-      '/apps/a.ts',
-      '/apps/b.js',
-      '/apps/.c.ts',
-      '/apps/node_modules/a.ts',
-      '/apps/dist/a.ts',
-      '/apps/test/a.ts',
-      '/apps/__tests__/a.ts',
-      '/apps/__mocks__/a.ts',
-      '/apps/a.d.ts',
-      '/apps/a.test.ts',
-      '/apps/a.spec.ts',
-      '/apps/a.test.js',
-      '/apps/a.spec.js',
-      '/apps/a.test.mts',
-      '/apps/a.spec.mts',
-      '/apps/a.test.cts',
-      '/apps/a.spec.cts',
-      '/apps/a.test.tsx',
-      '/apps/a.spec.tsx',
-      '/packages/a.ts',
-      '/packages/b.js',
-      '/packages/.c.ts',
+      '/root/apps/a.ts',
+      '/root/apps/b.js',
+      '/root/apps/.c.ts',
+      '/root/apps/node_modules/a.ts',
+      '/root/apps/dist/a.ts',
+      '/root/apps/test/a.ts',
+      '/root/apps/__tests__/a.ts',
+      '/root/apps/__mocks__/a.ts',
+      '/root/apps/a.d.ts',
+      '/root/apps/a.test.ts',
+      '/root/apps/a.spec.ts',
+      '/root/apps/a.test.js',
+      '/root/apps/a.spec.js',
+      '/root/apps/a.test.mts',
+      '/root/apps/a.spec.mts',
+      '/root/apps/a.test.cts',
+      '/root/apps/a.spec.cts',
+      '/root/apps/a.test.tsx',
+      '/root/apps/a.spec.tsx',
+      '/root/packages/a.ts',
+      '/root/packages/b.js',
+      '/root/packages/.c.ts',
     ].forEach((file) => {
       fs.mkdirSync(path.dirname(file), { recursive: true });
       fs.writeFileSync(file, '');
     });
   });
 
+  const glob = async (include: string[], exclude: string[] = []) => {
+    const files = await getIncludedFiles(
+      {
+        include,
+        exclude: [...defaultExclude, ...exclude],
+      },
+      '/root/apps',
+      memfs,
+    );
+    // ensure consistent paths across platforms
+    return files
+      .map((file) => path.relative('/root', file).replaceAll(path.sep, '/'))
+      .sort();
+  };
+
   it('should include visible files by default', async () => {
     expect(
-      await getIncludedFiles(
-        {
-          include: ['**/*.{js,ts}', '../packages/*.{js,ts}'],
-          exclude: [...defaultExclude],
-        },
-        '/apps',
-        memfs,
-      ),
+      await glob(['**/*.{js,ts}', '../packages/*.{js,ts}']),
     ).toMatchInlineSnapshot(`
       [
-        "/apps/a.ts",
-        "/apps/b.js",
-        "/packages/a.ts",
-        "/packages/b.js",
+        "apps/a.ts",
+        "apps/b.js",
+        "packages/a.ts",
+        "packages/b.js",
       ]
     `);
   });
 
   it('should include hidden files if explicitly specified', async () => {
-    expect(
-      await getIncludedFiles(
-        {
-          include: ['**/*.{js,ts}', '**/.c.ts'],
-          exclude: [...defaultExclude],
-        },
-        '/apps',
-        memfs,
-      ),
-    ).toMatchInlineSnapshot(`
+    expect(await glob(['**/*.{js,ts}', '**/.c.ts'])).toMatchInlineSnapshot(`
       [
-        "/apps/.c.ts",
-        "/apps/a.ts",
-        "/apps/b.js",
+        "apps/.c.ts",
+        "apps/a.ts",
+        "apps/b.js",
       ]
     `);
   });
 
   it('should exclude node_modules, dist, test, __tests__, __mocks__ by default', async () => {
-    expect(
-      await getIncludedFiles(
-        {
-          include: ['**/*'],
-          exclude: [...defaultExclude],
-        },
-        '/apps',
-        memfs,
-      ),
-    ).toMatchInlineSnapshot(`
+    expect(await glob(['**/*'])).toMatchInlineSnapshot(`
       [
-        "/apps/a.ts",
-        "/apps/b.js",
+        "apps/a.ts",
+        "apps/b.js",
       ]
     `);
   });
 
   it('should exclude .d.ts files by default', async () => {
-    expect(
-      await getIncludedFiles(
-        {
-          include: ['**/*.d.ts'],
-          exclude: [...defaultExclude],
-        },
-        '/apps',
-        memfs,
-      ),
-    ).toMatchInlineSnapshot('[]');
+    expect(await glob(['**/*.d.ts'])).toMatchInlineSnapshot('[]');
   });
 
   it('should exclude test and spec files by default', async () => {
-    expect(
-      await getIncludedFiles(
-        {
-          include: ['**/*.{test,spec}.*'],
-          exclude: [...defaultExclude],
-        },
-        '/apps',
-        memfs,
-      ),
-    ).toMatchInlineSnapshot('[]');
+    expect(await glob(['**/*.{test,spec}.*'])).toMatchInlineSnapshot('[]');
   });
 });
