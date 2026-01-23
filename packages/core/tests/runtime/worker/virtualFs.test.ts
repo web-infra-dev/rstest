5d8272cdf530767b487fbc94c15731375a5481ff
ScriptedAlchemy <zackaryjackson@bytedance.com>
2026-01-23 12:15:37 -0800

    chore: fix cspell for virtual fs test

--- packages/core/tests/runtime/worker/virtualFs.test.ts
+++ packages/core/tests/runtime/worker/virtualFs.test.ts
@@ -5,11 +5,13 @@ import {
   installVirtualFs,
   setVirtualFiles,
   uninstallVirtualFs,
-} from '../../../src/runtime/worker/virtualFs';
+} from '../../../src/runtime/worker/virtualFs'; // cspell:disable-line
 
 describe('virtualFs', () => {
   it('intercepts fs reads for virtual files and restores originals on uninstall', async () => {
-    const tmpDir = fs.mkdtempSync(path.join(os.tmpdir(), 'rstest-virtualfs-'));
+    const tmpDir = fs.mkdtempSync(
+      path.join(os.tmpdir(), 'rstest-virtual-fs-'), // cspell:disable-line
+    );
     const realFile = path.join(tmpDir, 'real.txt');
     const virtualFile = path.join(tmpDir, 'virtual.txt');
 
