773ddb3c7ca80caef333c2f0cb9a119b74f01b01
9aoy <9aoyuao@gmail.com>
2026-02-10 15:20:45 +0800

    chore(deps): bump rspress 2.0 (#918)

--- website/theme/index.tsx
+++ website/theme/index.tsx
@@ -1,41 +1,13 @@
 import { useLang } from '@rspress/core/runtime';
-import {
-  Layout as BaseLayout,
-  getCustomMDXComponent as basicGetCustomMDXComponent,
-} from '@rspress/core/theme-original';
+import { Layout as BaseLayout } from '@rspress/core/theme-original';
 import {
   Search as PluginAlgoliaSearch,
   ZH_LOCALES,
 } from '@rspress/plugin-algolia/runtime';
-import {
-  LlmsContainer,
-  LlmsCopyButton,
-  LlmsViewOptions,
-} from '@rspress/plugin-llms/runtime';
 import { NavIcon } from '@rstack-dev/doc-ui/nav-icon';
 
 import { HomeLayout } from './pages';
 import './index.scss';
-
-export function getCustomMDXComponent() {
-  const { h1: H1, ...mdxComponents } = basicGetCustomMDXComponent();
-
-  const MyH1 = ({ ...props }) => {
-    return (
-      <>
-        <H1 {...props} />
-        <LlmsContainer>
-          <LlmsCopyButton />
-          <LlmsViewOptions />
-        </LlmsContainer>
-      </>
-    );
-  };
-  return {
-    ...mdxComponents,
-    h1: MyH1,
-  };
-}
 
 const Search = () => {
   const lang = useLang();
