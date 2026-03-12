1f132b56c2f6296b37d09781d0fc7c05fcbd650a
9aoy <9aoyuao@gmail.com>
2026-03-12 18:49:16 +0800

    feat: support table syntax for `test.each` (#1049)

--- e2e/test-api/for.test.ts
+++ e2e/test-api/for.test.ts
@@ -3,7 +3,7 @@ import { afterAll, expect, it } from '@rstest/core';
 const logs: string[] = [];
 
 afterAll(() => {
-  expect(logs.length).toBe(6);
+  expect(logs.length).toBe(9);
 });
 
 it.for([
@@ -21,5 +21,16 @@ it.for([
   [3, 1, 4],
 ])('case-%# add(%i, %i) -> %i', ([a, b, expected], { expect }) => {
   expect(a! + b!).toBe(expected);
+  logs.push('executed');
+});
+
+// Template table syntax
+it.for<{ a: number; b: number; expected: number }>`
+  a    | b    | expected
+  ${1} | ${2} | ${3}
+  ${2} | ${3} | ${5}
+  ${4} | ${5} | ${9}
+`('template for add($a, $b) -> $expected', ({ a, b, expected }, { expect }) => {
+  expect(a + b).toBe(expected);
   logs.push('executed');
 });
