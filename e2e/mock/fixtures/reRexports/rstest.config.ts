7ec35aadcce825ca2e8a0bb1f1e65fe24a564d40
9aoy <9aoyuao@gmail.com>
2026-03-18 17:22:21 +0800

    chore: remove `mocking re-exported modules` tip

--- e2e/mock/fixtures/reRexports/rstest.config.ts
+++ e2e/mock/fixtures/reRexports/rstest.config.ts
@@ -4,11 +4,4 @@ import { defineConfig } from '@rstest/core';
 export default defineConfig({
   testEnvironment: 'jsdom',
   plugins: [pluginReact()],
-  tools: {
-    rspack: {
-      optimization: {
-        providedExports: false,
-      },
-    },
-  },
 });
