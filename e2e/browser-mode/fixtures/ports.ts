67d1ebda6e0171d2df13c3542d34d44781464001
9aoy <9aoyuao@gmail.com>
2026-05-06 19:05:36 +0800

    fix: port

--- e2e/browser-mode/fixtures/ports.ts
+++ e2e/browser-mode/fixtures/ports.ts
@@ -30,7 +30,7 @@ export const BROWSER_PORTS = {
   'github-actions': 5224,
   silent: 5230,
   'browser-coverage-multiproject': 5228,
-  related: 5230,
+  related: 5232,
 } as const;
 
 const browserPortValues = Object.values(BROWSER_PORTS);
